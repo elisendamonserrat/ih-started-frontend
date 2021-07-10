556ffe1bf5d9d48139791cd116e38dc47904834a
zapatran <zapatran@users.noreply.github.com>
2021-07-10 14:13:01 +0200

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
+import AnonRoute from './components/AnonRoute';
+import Private from './pages/Private';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import { withAuth } from './providers/AuthProvider';
+
+class App extends Component {
+	render() {
+		const { isLoading } = this.props;
+		if (isLoading) {
+			return <div>loading ... </div>;
+		}
+		return (
+			<div className="container">
+				<h1>Basic React Authentication</h1>
+				<Navbar />
+				<Switch>
+					<AnonRoute path="/signup" component={Signup} />
+					<AnonRoute path="/login" component={Login} />
+					<PrivateRoute path="/private" component={Private} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
-export default App;
+export default withAuth(App);
